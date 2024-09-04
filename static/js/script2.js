f44d8a23e93ba28795b4777e6b707ae9eef4a0f5
amrrmohamad <amr.r.mohamad55@gmail.com>
2024-09-04 22:41:10 +0300

    update

--- static/js/script2.js
+++ static/js/script2.js
@@ -2,9 +2,10 @@ document.getElementById('dark-mode-toggle').addEventListener('click', function()
     // Toggle dark mode for the entire page
     document.body.classList.toggle('dark-mode');
     document.querySelector('header').classList.toggle('dark-mode');
-    document.querySelector('footer').classList.toggle('dark-mode');
-    document.querySelector('.container').classList.toggle('dark-mode');
-    document.querySelectorAll('.repo').forEach(repo => repo.classList.toggle('dark-mode'));
+    document.querySelectorAll('.nav-links li a').forEach(link => link.classList.toggle('dark-mode'));
+    document.querySelectorAll('.button-header').forEach(button => button.classList.toggle('dark-mode'));
+    document.querySelector('.search-bar input').classList.toggle('dark-mode');
+    document.querySelector('.search-button').classList.toggle('dark-mode');
 
     // Toggle dark mode for the GitHub calendar
     const calendar = document.querySelector('#github-calendar');
